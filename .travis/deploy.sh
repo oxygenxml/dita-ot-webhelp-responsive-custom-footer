1b65723027b8e58478e6cf330e2896a3f5e6a30e
Radu Pisoi <radupisoi23@gmail.com>
2017-09-17 19:36:40 +0300

    Publish to gh-pages

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -18,9 +18,9 @@ git config user.email "support@oxygenxml.com"
 
 git remote add upstream "https://$WH_GH_TOKEN@github.com/oxygenxml/com.oxygenxml.webhelp.responsive.custom.footer.git"
 git fetch upstream
-git reset upstream/publish
+git reset upstream/gh-pages
 
 git add -A .
 git status
 git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:publish
+git push -q upstream HEAD:gh-pages
