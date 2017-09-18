2ac80e1d6383dd6289df941eeb3c02f8935001b6
oXygen XML Deployer <support@oxygenxml.com>
2017-09-18 11:44:11 +0000

    rebuild pages at 7553b98

---
+++ it-book/oxygen-webhelp/search/searchOptions.js
@@ -0,0 +1,11 @@
+// Expanded from webhelp.search.ranking ANT property
+var webhelpSearchRanking = true;
+
+// Expanded from webhelp.enable.search.autocomplete ANT property
+var webhelpEnableSearchAutocomplete = true;
+
+// Expanded from webhelp.search.enable.pagination ANT property
+var webhelpEnableSearchPagination = true;
+
+// Expanded from webhelp.search.page.numberOfItems ANT property
+var webhelpSearchNumberOfItems = 10;
