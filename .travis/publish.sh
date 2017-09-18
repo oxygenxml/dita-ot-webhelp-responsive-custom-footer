93716853e38b3f90bc213f6ab4f48e579785b56f
radu-pisoi <radu_pisoi@VM10.SYNCROSOFT>
2017-09-18 14:32:32 +0300

    modify publishing script

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -51,7 +51,7 @@ echo "Transform the sample to WebHelp Responsive output"
 echo "====================================="
 
 export ANT_OPTS=-Xmx1524m
-sh dita-ot-2.5.3/bin/dita -i -v resources/sample/it-book/taskbook.ditamap -f webhelp-responsive -output=publishing/it-book
+sh dita-ot-2.5.3/bin/dita -i resources/sample/it-book/taskbook.ditamap -f webhelp-responsive -output=publishing/it-book
 
 cp resources/gh-pages/index.html publishing/index.html
 echo "====================================="
